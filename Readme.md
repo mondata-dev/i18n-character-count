36cb7c6f9d394996465f30f48075d370e37eaecc
Christian Meyer <christian.meyer@mondata.de>
2020-10-15 15:34:16 +0200

    initial

---
+++ Readme.md
@@ -0,0 +1,13 @@
+# i18n-character-count
+
+Very simple character counter for counting up characters in an i18n system that is based 
+on js objects in js files. This is very rudimentary and might break easily.
+
+### How to run
+
+npm run count *myFilePaths* [-- -e *excludeFileNames*]
+
+replace *myFilePaths* with any number of paths to files or directories. 
+
+If a directory is given, all files inside that directory will be parsed.
+To exclude certain files, add the **-e** flag. Don't forget **--**. 
---
+++ index.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+const path = require('path');
+const argv = require('minimist')(process.argv.slice(2));
+
+console.log(argv);
+
+const countString = function (acc, s) {
+    if (typeof s === "string") {
+        return acc + s.length;
+    } else if (typeof s === "object" && s !== null) {
+        return Object.values(s).reduce((acc, v) => {
+            return countString(acc, v);
+        }, acc)
+    } else {
+        return acc;
+    }
+}
+
+const countFile = function (path) {
+    try {
+        console.log('parsing ', path);
+        let obj = require(path);
+        const count = countString(0, obj);
+        console.log(count);
+        return count;
+    } catch (e) {
+        console.error(e);
+        return 0;
+    }
+}
+
+const paths = argv._;
+const exclude = argv.e || argv.exclude || [];
+let count = 0;
+paths.forEach(async p => {
+    if (fs.lstatSync(p).isDirectory()) {
+        fs.readdirSync(p).forEach(f => {
+            if (!exclude.includes(f)) {
+                count += countFile(path.join(p, f));
+            } else {
+                console.log('skipping: ', f);
+            }
+        })
+    } else {
+        count += countFile(p);
+    }
+})
+console.log('total character count: ', count);
